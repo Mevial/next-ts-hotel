25ba8e4cbd020c60b6c52121a3d36415c987d649
Max <61512470+Mevial@users.noreply.github.com>
2022-01-13 15:45:54 +0300

    add eslint

--- express/index.ts
+++ express/index.ts
@@ -1,11 +1,11 @@
-import express from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import dotenv from 'dotenv';
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.get("/", (request, response) => {
-  response.send("Express + TS");
+app.get('/', (request, response) => {
+  response.send('Express + TS');
 });
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
